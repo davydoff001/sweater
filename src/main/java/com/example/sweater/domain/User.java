89e628c59a215ebbbd762f5c9a0722a068a6af17
a.davydov <a.davydov@KV-KYI-CRP-M131.ukravto.loc>
2019-02-23 12:37:33 +0200

    Подписки и подписчики.

--- src/main/java/com/example/sweater/domain/User.java
+++ src/main/java/com/example/sweater/domain/User.java
@@ -6,6 +6,7 @@
 package com.example.sweater.domain;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import javax.persistence.*;
@@ -40,6 +41,22 @@ public class User implements UserDetails {
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Message> messages;
 
+    @ManyToMany
+    @JoinTable(
+            name = "user_subscriptions",
+            joinColumns = { @JoinColumn(name="channel_id") },
+            inverseJoinColumns = { @JoinColumn(name="subscriber_id") }
+    )
+    private Set<User> subscribers = new HashSet<>();
+    
+    @ManyToMany
+    @JoinTable(
+            name = "user_subscriptions",
+            joinColumns = { @JoinColumn(name="subscriber_id") },
+            inverseJoinColumns = { @JoinColumn(name="channel_id") }
+    )
+    private Set<User> subscriptions = new HashSet<>();
+    
     @Override
     public int hashCode() {
         int hash = 7;
@@ -155,6 +172,22 @@ public class User implements UserDetails {
 
     public void setMessages(Set<Message> messages) {
         this.messages = messages;
+    }
+
+    public Set<User> getSubscribers() {
+        return subscribers;
+    }
+
+    public void setSubscribers(Set<User> subscribers) {
+        this.subscribers = subscribers;
+    }
+
+    public Set<User> getSubscriptions() {
+        return subscriptions;
+    }
+
+    public void setSubscriptions(Set<User> subscriptions) {
+        this.subscriptions = subscriptions;
     }
     
     
