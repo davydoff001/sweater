cf6cd5edd41f3772cf329f94c5b2d8a9cd545ffc
a.davydov <a.davydov@KV-KYI-CRP-M131.ukravto.loc>
2019-03-28 11:14:59 +0200

    Добавлено ускорение отображения страниц (Turbolinks).

--- src/main/java/com/example/sweater/config/MvcConfig.java
+++ src/main/java/com/example/sweater/config/MvcConfig.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import util.RedirectInterceptor;
 /**
  *
  * @author a.davydov
@@ -41,5 +43,9 @@ public class MvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/static/");
     }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RedirectInterceptor());
+    }
     
 }
---
+++ src/main/java/util/RedirectInterceptor.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ *
+ * @author a.davydov
+ */
+public class RedirectInterceptor extends HandlerInterceptorAdapter{
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object obj, ModelAndView mav) throws Exception {
+        if (mav != null) {
+            String args = request.getQueryString() != null ? request.getQueryString() : "";
+            String url = request.getRequestURI().toString() + "?" + args;
+            response.setHeader("Turbolinks-Location", url);
+        }
+    }
+    
+}
